5bfa38d05bef580f4bdc5926f32d80736ae30b3e
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-02-17 05:50:05 +0530

    Add Hot Reload via vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,50 @@
 import react from "@vitejs/plugin-react";
+import { readFile, writeFile } from "fs/promises";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
-export default defineConfig({
-    plugins: [react(), viteSingleFile()],
-    build: {
-        target: "esnext",
-        assetsInlineLimit: 100000000, // Force everything inline
-        chunkSizeWarningLimit: 100000000,
-        cssCodeSplit: false, // Don't split CSS
-        emptyOutDir: false,
-        outDir: "dist",
-        rollupOptions: {
-            input: {
-                ui: "./index.html", // Your React entry HTML
-            },
-            output: {
-                entryFileNames: "ui.js", // Output specific name
-            },
+function build() {
+    return {
+        name: "build",
+        async buildStart() {
+            const manifest = await readFile(
+                resolve(__dirname, "manifest.json"),
+                "utf-8",
+            );
+            await writeFile(
+                resolve(__dirname, "dist/manifest.json"),
+                manifest,
+                "utf-8",
+            );
         },
-    },
+    };
+}
+
+export default defineConfig(() => {
+    return {
+        plugins: [
+            react(),
+            viteSingleFile({
+                useRecommendedBuildConfig: false,
+                removeViteModuleLoader: true,
+            }),
+            build(),
+        ],
+        build: {
+            target: "esnext",
+            assetsInlineLimit: 100000000,
+            chunkSizeWarningLimit: 100000000,
+            cssCodeSplit: false,
+            emptyOutDir: false,
+            inlineDynamicImports: false,
+            outDir: "dist",
+            rollupOptions: {
+                input: {
+                    ui: resolve(__dirname, "index.html"),
+                },
+            },
+            watch: {},
+        },
+    };
 });
